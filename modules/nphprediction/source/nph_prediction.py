9f5de6bb47d2946bcaeb26be36d55ddbb71d4ef7
Amil Khan <amil@umail.ucsb.edu>
2020-05-12 14:08:25 -0700

    Angela's NPH Prediction Module

---
+++ modules/nphprediction/source/nph_prediction.py
@@ -0,0 +1,70 @@
+###############################################################################
+##  Vision Research Laboratory and                                           ##
+##  Center for Multimodal Big Data Science and Healthcare                    ##
+##  University of California at Santa Barbara                                ##
+## ------------------------------------------------------------------------- ##
+##                                                                           ##
+##     Copyright (c) 2019                                                    ##
+##     by the Regents of the University of California                        ##
+##                            All rights reserved                            ##
+##                                                                           ##
+## Redistribution and use in source and binary forms, with or without        ##
+## modification, are permitted provided that the following conditions are    ##
+## met:                                                                      ##
+##                                                                           ##
+##     1. Redistributions of source code must retain the above copyright     ##
+##        notice, this list of conditions, and the following disclaimer.     ##
+##                                                                           ##
+##     2. Redistributions in binary form must reproduce the above copyright  ##
+##        notice, this list of conditions, and the following disclaimer in   ##
+##        the documentation and/or other materials provided with the         ##
+##        distribution.                                                      ##
+##                                                                           ##
+##                                                                           ##
+## THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> "AS IS" AND ANY           ##
+## EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE         ##
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR        ##
+## PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR           ##
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     ##
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       ##
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        ##
+## PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    ##
+## LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      ##
+## NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        ##
+## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              ##
+##                                                                           ##
+## The views and conclusions contained in the software and documentation     ##
+## are those of the authors and should not be interpreted as representing    ##
+## official policies, either expressed or implied, of <copyright holder>.    ##
+###############################################################################
+
+import sys
+import argparse
+import os
+#from source  import imUtils, segUtils, postUtils, unetSeg
+from imUtils import *
+from segUtils import *
+from postUtils import *
+from unetSeg import *
+
+
+
+
+
+def main(base='source/', parallel=False):
+	threshold(base, 'Scans', parallel)
+	unetPredict(base)
+	subarachnoid_seg(base, parallel)
+	get_volumes(base)
+	make_prediction(base)
+	clean_up(base)
+	
+  
+if __name__== "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--directory', default='')
+	parser.add_argument('--parallel', action='store_true', default=False)
+	args = parser.parse_args()
+	main(args.directory, args.parallel)
+
+
