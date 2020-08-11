893d8c434bb636dc28860c1ece5fc990594874cd
amilworks <amil@ucsb.edu>
2020-08-11 12:54:56 -0700

    Removed virtual environment references

--- source/bqcore/bq/util/commands.py
+++ source/bqcore/bq/util/commands.py
@@ -7,9 +7,9 @@ def asbool(v):
 
 def find_site_cfg(cfg_file):
     paths = list(config_dirs)
-    # Also check if we are in standard virtualenv layout
-    if os.environ.get('VIRTUAL_ENV', '').endswith('bqenv'):
-        paths.append (os.path.join (os.path.dirname (os.environ.get ('VIRTUAL_ENV')), 'config'))
+    # Also check if we are in standard virtualenv layout REMOVED BY YOURS TRULY
+    # if os.environ.get('VIRTUAL_ENV', '').endswith('bqenv'):
+    #     paths.append (os.path.join (os.path.dirname (os.environ.get ('VIRTUAL_ENV')), 'config'))
     for dp in paths:
         site_cfg = os.path.join(dp, cfg_file)
         if os.path.exists(site_cfg):
--- start-bisque.sh
+++ start-bisque.sh
@@ -2,4 +2,4 @@
 cd /source
 bq-admin server start
 sleep 10
-tail -f bisque_8080.log
+# tail -f bisque_8080.log
