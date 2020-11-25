7c6a79246116c220c410d38060f1662b4f666652
amilworks <amil@ucsb.edu>
2020-11-24 22:10:56 -0800

    Updated docker install

--- builder/install-docker.sh
+++ builder/install-docker.sh
@@ -3,10 +3,10 @@ set -x
 
 echo "Installing docker client"
 
-wget -q https://download.docker.com/linux/static/stable/x86_64/docker-19.03.12.tgz 
-tar xzf docker-19.03.12.tgz docker/docker
+wget -q  https://get.docker.com/builds/Linux/x86_64/docker-17.03.0-ce.tgz
+tar xzf docker-17.03.0-ce.tgz docker/docker
 mv docker/docker /usr/bin/docker
-rm -rf docker-19.03.12.tgz docker/
+rm -rf docker-17.03.0-ce.tgz docker/
 
 #wget -q get-docker.com -O get-docker.sh
 #sh get-docker.sh
